ecc775797ec1700e1549680572a1dcd9e6de92ee
Miles Bramwit <mbb249@cornell.edu>
2023-12-07 12:43:24 -0500

    Psystem largelty done

--- src/FinalProject/Examples/Nodes/MyParticleSystem/DirectionalParticleSystemModel.ts
+++ src/FinalProject/Examples/Nodes/MyParticleSystem/DirectionalParticleSystemModel.ts
@@ -1,3 +1,4 @@
+import { random } from "tinycolor2";
 import { ASerializable, Vec3, Color } from "../../../../anigraph";
 import { AInstancedParticleSystemModel } from "../../../../anigraph/effects";
 import { SphereParticle } from "./SphereParticle";
@@ -5,65 +6,93 @@ import { SphereParticle } from "./SphereParticle";
 
 @ASerializable("DirectionalParticleSystemModel")
 export class DirectionalParticleSystemModel extends AInstancedParticleSystemModel<SphereParticle>{
-  // Define additional properties if needed
+  gravity: Vec3 = new Vec3(0, 0, -.0005); // Gravity vector
 
-  constructor(nParticles?: number, ...args: any[]) {
+  lastEmittedIndex: number = 0;
+  timesRun: number = 0;
+
+  constructor(nParticles: number = 50) {
     super(nParticles);
-    this.initParticles(nParticles = 100);
+    this.initParticles(nParticles);
     this.signalParticlesUpdated();
   }
 
   initParticles(nParticles: number) {
     for (let i = 0; i < nParticles; i++) {
       let newParticle = new SphereParticle();
-      newParticle.visible = false; // Initialize as invisible
+      newParticle.visible = false;
       this.addParticle(newParticle);
     }
   }
 
-  addParticle(particle: SphereParticle) {
-    this.particles.push(particle);
-  }
+  emit(mass?: number, radius?: number, t0: number = -1) {
+    let i = (this.lastEmittedIndex + 1) % this.nParticles;
 
-  emitParticlesAtLocation(location: Vec3, leftDirection: Vec3, rightDirection: Vec3, count = 10, lifespan = 3) {
-    for (let i = 0; i < count / 2; i++) {
-      let upwardForce = 0.5;
-      let leftVelocity = new Vec3(leftDirection.x * Math.random(), leftDirection.y * Math.random(), upwardForce);
-      let rightVelocity = new Vec3(rightDirection.x * Math.random(), rightDirection.y * Math.random(), upwardForce);
 
-      let leftParticle = new SphereParticle(location, leftVelocity, lifespan);
-      let rightParticle = new SphereParticle(location, rightVelocity, lifespan);
+    let random_y = Math.random() * 0.3
+    let random_x = Math.random() * 0.3
 
-      this.addParticle(leftParticle);
-      this.addParticle(rightParticle);
+
+    this.particles[i].position = new Vec3(random_x, random_y, 0);
+
+    let ran_num = Math.random()
+
+    if (ran_num > 0.75) {
+      this.particles[i].velocity = new Vec3(-0.01, 0, .025)
+    } else if (ran_num > 0.5) {
+      this.particles[i].velocity = new Vec3(0, 0.01, .025)
     }
+    else if (ran_num > 0.25) {
+      this.particles[i].velocity = new Vec3(0, -0.01, .025)
+    } else {
+      this.particles[i].velocity = new Vec3(.01, 0, .025)
+    }
+
+
+    this.particles[i].mass = mass ?? 1;
+    this.particles[i].size = radius ?? 0.1;
+    this.particles[i].visible = true;
+    this.particles[i].t0 = t0;
+
+
+    this.particles[i].color = Color.Red()
+
+    this.lastEmittedIndex = i;
   }
 
-  timeUpdate(t: number, ...args: any[]) {
+  timeUpdate(t: number) {
     super.timeUpdate(t);
 
-    for (let i = this.particles.length - 1; i >= 0; i--) {
-      let particle = this.particles[i];
 
-      particle.position.plus(particle.velocity);
-
-
-      particle.lifespan -= t;
-
-      if (particle.lifespan <= 0) {
-
-        this.particles.splice(i, 1);
-      } else {
-
-        let lifespanFraction = particle.lifespan / particle.initialLifespan;
-        particle.radius = particle.initialRadius * lifespanFraction;
+    if (this.timesRun < 50) {
+      for (let i = 0; i < 1; i++) {
 
 
-
+        this.emit();
       }
 
     }
 
+
+    for (let i = 0; i < this.particles.length; i++) {
+      let p = this.particles[i];
+      p.position = p.position.plus(
+        p.velocity
+      );
+
+      p.velocity = p.velocity.plus(this.gravity);
+
+      if (p.position.z <= 0) {
+        p.visible = false
+      }
+    }
+
     this.signalParticlesUpdated();
+
+    this.timesRun += 1
   }
+
+
+
 }
+
--- src/FinalProject/Examples/Nodes/MyParticleSystem/SphereParticle.ts
+++ src/FinalProject/Examples/Nodes/MyParticleSystem/SphereParticle.ts
@@ -2,35 +2,14 @@ import { AParticle3D } from "../../../../anigraph";
 import { Color, Vec3 } from "../../../../anigraph";
 
 export class SphereParticle extends AParticle3D {
-    radius: number;
-    color: Color;
-    initialRadius: number;
-    lifespan: number;
-    initialLifespan: number;
+    color!: Color;
+    t0: number = 0;
 
-    constructor(position?: Vec3, velocity?: Vec3, radius = 5, mass?: number, lifespan = 10) {
-        super(position, velocity, mass);
-        this.radius = radius;
-        this.initialRadius = radius;
-        this.color = Color.FromRGBA(0, 1, 0, 1);
-        this.lifespan = lifespan;
-        this.initialLifespan = lifespan;
+    constructor(position?: Vec3, velocity?: Vec3, mass?: number, size?: number, color?: Color) {
+        super(position, velocity, mass, size);
+        this.color = color ?? Color.FromString("#00ff00");
     }
 
-    updateSizeOverTime(deltaTime: number) {
-
-        this.lifespan -= deltaTime;
 
 
-        if (this.lifespan > 0) {
-            let lifespanFraction = this.lifespan / this.initialLifespan;
-            this.radius = this.initialRadius * lifespanFraction;
-        } else {
-            this.radius = 0;
-        }
-    }
-
-    get opacity() {
-        return this.color.a;
-    }
 }
